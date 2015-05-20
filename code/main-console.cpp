d5da0941c0f35db54f3863c2aff17394ed77501b
Mila Brandt <mail@mila-brandt.de>
2015-05-20 22:17:46 +0200

    created basic project structure, updated cmake
    commented the gui part, because it does not compile
    close #2

---
+++ code/main-console.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+int main(int argc, char** argv){
+
+	if (argc < 2) {
+		std::cerr << "Usage Error, at least one argument is required!"<< std::endl;
+		return 1;
+	}
+
+	return 0;
+}
+
+
