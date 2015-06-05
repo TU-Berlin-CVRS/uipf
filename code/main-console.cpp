2565c0ec07ab0a3fa372ed77b65daa1948d1a726
Mila Brandt <mail@mila-brandt.de>
2015-06-05 13:07:01 +0200

    created basic structure for console interface
    works only with one module.
    
    TODO: use metadata from modules + use boost

--- code/main-console.cpp
+++ code/main-console.cpp
@@ -15,21 +15,64 @@ int main(int argc, char** argv){
 		std::cerr << "Usage: " << argv[0] << " <configFile>"<< std::endl;
 		return 1;
 	}
+	
+	// run a processing chain from a config file.
+	// ./uipf -c example.yam
+	if (string(argv[1]).compare("-c") == 0){
+		
+		
+		// QUESTION: for what is this used??
+		QApplication app (argc,argv);
 
-	QApplication app (argc,argv);
+		// loads the configFile and create a Configuration
+		Configuration conf;
+		string configFileName = argv[2];
+		conf.load(configFileName);
 
-	// loads the configFile and create a Configuration
-	Configuration conf;
-	string configFileName = argv[1];
-	conf.load(configFileName);
+		// only for debug, print the loaded config
+		conf.store("test.yaml");
+		
+	} else{
+	// run a single module.
+	// ./uipf <moduleName> ...options...		
+	// TODO finish it
 
-	// only for debug, print the loaded config
-	conf.store("test.yaml");
+		// loads the configFile and create a Configuration
+		Configuration conf;
+
+		// TODO if there are more than one input file, this step should be repeated
+		ProcessingStep loadModule;
+		loadModule.name = "loadModule";
+		// TODO: check whether it is the correct name of the module and correct name of the param
+		loadModule.module = "loadImage";	
+		loadImage.params.insert (pair<string,string>("filename",???) ); // fill from -i
+
+		// TODO if there are more than one output file, this step should be repeated
+		ProcessingStep storeModule;
+		storeModule.name = "storeModule";
+		// TODO: check whether it is the correct name of the module and correct name of the param
+		storeModule.module = "storeImage";	
+		storeImage.input.insert (pair<string, pair<string, string> >("image",???) ); 	// fill from module's outputs
+		storeImage.params.insert (pair<string,string>("filename",???) ); 				// fill from -o, if empty, create automatically
+
+		ProcessingStep processModule;
+		processModule.name = "processModule";
+		processModule.module = argv[2];	
+		// TODO make a loop
+		process.input.insert (pair<string, pair<string, string> >(???,???) ); 	// fill from -i
+		process.params.insert (pair<string,string>("filename",???) ); 			// fill from -p
+
+		// TODO maybe a loop
+		conf.addProcessingStep(loadModule);
+		conf.addProcessingStep(storeModule);
+		conf.addProcessingStep(processModule);
+	}
 	
 	// loads the Configuration and runs it
 	ModuleManager mm(conf);
 	mm.run();	
-
+	
+	
 	return 0;
 }
 
