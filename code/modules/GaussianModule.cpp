7bfdea8286d9bf7280f05bcdb16a4c4836cc9e19
Marcus Zepp <marcus.zepp@googlemail.com>
2015-06-09 00:43:12 +0200

    implemented simple gaussian blur

--- code/modules/GaussianModule.cpp
+++ code/modules/GaussianModule.cpp
@@ -1,4 +1,5 @@
-#include <iostream>
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/highgui/highgui.hpp"
 #include "GaussianModule.hpp"
 
 using namespace std;
@@ -7,18 +8,24 @@ using namespace uipf;
 // executes the Module
 /*
 input 		is a std::map of input resources, the names are described in the module meta description
-params 		is a std::map of input paramaeters, the names are described in the module meta description
+params 		is a std::map of input parameters, the names are described in the module meta description
 ouput 		is a std::map of output resources, the names are described in the module meta description
 context 	is a container providing access to the current environment, allowing to open windows, write to logger etc...
 */
 void GaussianModule::run(map<string, Data::ptr& >& input, map<string, string>& params, map<string, Data::ptr>& output ) const
 {
+        using namespace cv;
+        
 	listParams(params);
 	Matrix* oMatrix = getData<Matrix>(input,"image");
 
-	// TODO implement actual gaussian
-
-	cv::Mat m = oMatrix->getContent().clone();
+        int nWindowSize = getParam<int>(params,"windowSize",-1);
+        double dSigma = getParam<double>(params,"sigma",0.0);
+     
+	cv::Mat m = oMatrix->getContent(); //2DO cloning whith clear output naming
+	
+	GaussianBlur(m,m,Size( nWindowSize, nWindowSize ), dSigma, dSigma );
+	//2DO naming unclear: is it just "image" or "process.image"??? What wins: Metadata or yaml-config-file?
 	output.insert (pair < string, Data::ptr >("image", Matrix::ptr(new Matrix(m))));
 }
 
