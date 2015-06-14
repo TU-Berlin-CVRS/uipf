f87adca96457909419f603289f8f815f295d394a
zeppfisj <7epp@gmx.de>
2015-06-14 22:42:21 +0200

    shared pointer issue #56

--- code/framework/StdIncl.hpp
+++ code/framework/StdIncl.hpp
@@ -9,8 +9,7 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 
-//2DO change to boost::shared_ptr
-#define SMARTPOINTER std::unique_ptr
+#define SMARTPOINTER std::shared_ptr
 
 #endif //STDINCL
 
