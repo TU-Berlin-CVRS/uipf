82b82a64acf3ca9d47587ef756d8f4347b649201
zeppfisj <7epp@gmx.de>
2015-06-14 17:32:13 +0200

    moved methods to DataManager

--- code/framework/ModuleBase.cpp
+++ code/framework/ModuleBase.cpp
@@ -24,16 +24,7 @@ std::string ModuleBase::name() const
 	return name_;
 }
 
-void ModuleBase::listParams( map < string, string >& params) const
-{
- Logger::instance()->Info("Module has been executed with the following params:");
 
-  map < string, string >::iterator it = params.begin ();
-  for (; it != params.end (); ++it)
-  {
-    Logger::instance()->Info("    " + it->first + " - " + it->second );
-  }
-}
 
 
 }
