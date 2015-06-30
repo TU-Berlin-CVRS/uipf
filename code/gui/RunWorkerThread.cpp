561c4f5cae7062f86c8eb319fb39e53515553cb1
zeppfisj <7epp@gmx.de>
2015-06-30 21:55:00 +0200

    implemented a Backgroundworker for processing chain in a different thread. issue #84

---
+++ code/gui/RunWorkerThread.cpp
@@ -0,0 +1,32 @@
+/*
+ * RunWorkerThread.cpp
+ *
+ *  Created on: 30 Jun 2015
+ *      Author: uipf
+ */
+
+#include "RunWorkerThread.h"
+#include "../framework/GUIEventDispatcher.h"
+#include "../framework/Logger.hpp"
+
+using namespace uipf;
+
+RunWorkerThread::RunWorkerThread(ModuleManager& mm, Configuration& config)
+ : mm_(mm),config_(config)
+{
+
+}
+
+RunWorkerThread::~RunWorkerThread() {
+}
+
+void RunWorkerThread::stop()
+{
+	mm_.requestStop();
+}
+void RunWorkerThread::run()
+{
+	mm_.run(config_);
+	emit finished();
+}
+
---
+++ code/gui/RunWorkerThread.h
@@ -0,0 +1,36 @@
+#ifndef RUNWORKERTHREAD_H_
+#define RUNWORKERTHREAD_H_
+#include <QThread>
+#include "../framework/Configuration.hpp"
+#include "../framework/ModuleManager.hpp"
+
+using namespace uipf;
+
+//A class that does the background work in a thread.
+//By deriving QThread QT handles signals/slot thread marshaling automatically
+//for this all Types used in signals have to be registered by qRegisterMetaType() -> see GUIEventDispatcher.cpp
+class RunWorkerThread: public QThread {
+
+	Q_OBJECT
+
+public:
+	RunWorkerThread(ModuleManager& mm, Configuration& config);
+	virtual ~RunWorkerThread();
+
+	//run module chain in a separate thread
+	void run() Q_DECL_OVERRIDE;
+	//tell modules to stop work now
+	void stop();
+
+	// Define signal:
+ signals:
+ 	 //processing has finished -> tell GUI
+	void finished();
+
+private:
+
+	uipf::ModuleManager& mm_;
+	uipf::Configuration& config_;
+};
+
+#endif /* RUNWORKERTHREAD_H_ */
