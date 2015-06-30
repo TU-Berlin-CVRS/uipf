9dbd07c6a37ec9800ca1db59135aeb7b5e6258ee
zeppfisj <7epp@gmx.de>
2015-06-30 21:56:45 +0200

    added a long running task for testing

---
+++ code/modules/LongRunningDummyModule.hpp
@@ -0,0 +1,32 @@
+#ifndef _LONGRUNNIGDUMMYMODULE_
+#define _LONGRUNNIGDUMMYMODULE_
+
+#include "../framework/ModuleInterface.hpp"
+#include "../framework/ModuleBase.hpp"
+#include <QObject>
+#include <QtPlugin>
+
+namespace uipf{
+
+
+class LongRunningDummyModule :  public QObject,ModuleBase
+{
+Q_OBJECT
+Q_PLUGIN_METADATA(IID "org.tu-berlin.uipf.ModuleInterface" )
+Q_INTERFACES(uipf::ModuleInterface)
+
+	public:
+		// constructor tells ModuleBase our name so we don't need to implement name()
+		LongRunningDummyModule(void):ModuleBase("LongRunningDummyModule"){};
+
+		// destructor needs to be virtual otherwise it not called due polymorphism
+		virtual ~LongRunningDummyModule(void){};
+
+		void run( DataManager& data ) const Q_DECL_OVERRIDE;
+
+		uipf::MetaData getMetaData() const Q_DECL_OVERRIDE;
+};
+
+}
+
+#endif //_LONGRUNNIGDUMMYMODULE_
