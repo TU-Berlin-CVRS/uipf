972e0e3ed663a709178f8f62645b32ae38d06fb9
zeppfisj <7epp@gmx.de>
2015-06-30 21:57:51 +0200

    added signal to stop running modules

--- code/framework/Context.cpp
+++ code/framework/Context.cpp
@@ -9,6 +9,7 @@ using namespace uipf;
 void Context::displayImage(const std::string strTitle, const Matrix& oMat, bool bBlocking) const
 {
 	//2DO:Use QT
+	//2DO eliminate Warnings: "QApplication: Object event filter cannot be in a different thread."
 	using namespace cv;
 	namedWindow( strTitle.c_str(), WINDOW_AUTOSIZE );
 	imshow( strTitle.c_str(), oMat.getContent() );
