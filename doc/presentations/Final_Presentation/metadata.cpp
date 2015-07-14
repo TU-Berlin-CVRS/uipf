4f4b38b941de44297c33fb638ecd4d5cced52dd7
Carsten Brandt <mail@cebe.cc>
2015-07-14 13:11:39 +0200

    inserted module part
    Conflicts:
    	doc/presentations/Final_Presentation/finalPresentation.tex

--- doc/presentations/Final_Presentation/metadata.cpp
+++ doc/presentations/Final_Presentation/metadata.cpp
@@ -1,16 +1,13 @@
-MetaData LoadImageModule::getMetaData() const {
-	map<string, DataDescription> output = {
-		{"image", DataDescription(MATRIX, "loaded image.") }
-	};
-	map<string, ParamDescription> params = {
-		{"filename", ParamDescription("file name ...") },
-		{"mode", ParamDescription("can be either 'color' or 'grayscale'... Defaults to 'color'.",true) },
-	};
-	return MetaData(
-		"Load an image from a file.",
-		"I/O",
-		map<string, DataDescription>(), // no inputs
-		output,
-		params
-	);
+MetaData ResizeModule::getMetaData() const
+{
+    DataDescriptionMap input = {{"image", DataDescription(MATRIX, "the image to resize.") }	};
+
+    DataDescriptionMap output = {{"image", DataDescription(MATRIX, "the result image.") }};
+
+    ParamDescriptionMap params = {
+        {"width", ParamDescription("new width") },
+        {"height", ParamDescription("new height") }
+    };
+
+    return MetaData("Resizes an image using openCV.","Image Processing",input,output,params	);
 }
